2ed29e3c4b375a2c4b06dd10153073d5876cfa8c
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-27 14:43:24 +0000

    3rd test done, can add a tag to each note

--- lib/note.rb
+++ lib/note.rb
@@ -1,6 +1,10 @@
 class Note
-  attr_reader :content
+  attr_reader :content, :tag
   def initialize(content)
     @content = content
+    @tag = nil
+  end
+  def add_tag(tag)
+    @tag = tag
   end
 end
--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -5,4 +5,9 @@ describe 'note' do
     n = Note.new("Host meeting")
     expect(n.content).to eq "Host meeting"
   end
+  it 'stores a tag' do
+    n = Note.new("Host meeting")
+    n.add_tag("Important")
+    expect(n.tag).to eq "Important"
+  end
 end
